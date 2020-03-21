787e19a9d578f74632f204ff2947b4bab5aa0f12
ammar <ammar.khaskhoussy@esprit.tn>
2020-03-21 08:41:04 +0100

    somechanges

--- dari/src/main/java/com/example/repository/PictureRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.entity.*;
-
-public interface PictureRepository extends JpaRepository<Integer, Pictures> {
-
-}
